3e9abc601b342860e450cf34c3e4c24e91717593
Wes <wes@wes.(none)>
2013-09-06 20:46:20 -0500

    gather_all.py amended and running gathering all test data

--- topicstuff/gather_all.py
+++ topicstuff/gather_all.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
-from topic_classify import get_topics_and_words
+from topic_classify import get_topics_and_words2
 import json
 import os
+import mechanize, cookielib
 
 
 def gather_topics():
+
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_debug_http(True)
+    br.set_debug_redirects(True)
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
     global keep_going
-    f = open('/home/wes/Documents/projects/topic_classifier/topicstuff/topics.txt', 'r')
-    d = json.loads( f.read() )
+    f = open('topics.txt', 'r')
+    topics = json.loads( f.read() )['topics']
     f.close()
-    topics = d['topics']
     try:
-        failed = open('/home/wes/Documents/projects/topic_classifier/topicstuff/failed.txt', 'r').read().split('\n')
-        current = [ i.split('.')[0] for i in os.listdir('/home/wes/Documents/projects/topic_classifier/topicstuff/topstructs') ]
-        not_there = [ topic for topic in topics if topic not in current and topic not in failed ]
-        if len(not_there) > 0:
-            get_topics_and_words(not_there)
-        else:
-            keep_going = False
-    except:
-        gather_topics()
+        f2 = open('not_crawled.txt', 'r')
+        not_crawled = json.loads( f2.read() )
+    except IOError:
+        not_crawled = None
+    if not_crawled != None:
+        dont_crawl = not_crawled['topics']
+    else:
+        dont_crawl = None
+    crawled = [i.split('.')[0] for i in os.listdir('topstructs')]
+    to_crawl = [ topic for topic in topics if topic not in crawled ]
+    if dont_crawl != None:
+        to_crawl += [ topic for topic in topics if topic not in dont_crawl ]
+   
+    if len(to_crawl) > 0: 
+        for topic in to_crawl:
+            get_topics_and_words2(topic, dont_crawl, br)
+    else:
+        keep_going = False
+
 
 if __name__ == '__main__':
     keep_going = True
--- topicstuff/topic_classify.py
+++ topicstuff/topic_classify.py
@@ -1,6 +1,4 @@
-#     else:
-                keep_going = False
-       !/usr/bin/env python
+# !/usr/bin/env python
 import json
 import mechanize
 import re
@@ -74,8 +72,6 @@ def get_topics_and_words(topics):
                                     topic_struct["words"][word]["number"] += 1
                                 else:
                                     topic_struct["words"][word] = {"number": 1}
-                                    #data["topic"][topic]["words"][word] = {}
-                                    #data["topic"][topic]["words"][word]["number"] = 1
                 else:
                     pass
             f = open('/home/wes/Documents/projects/topic_classifier/topicstuff/topstructs/%s.txt' % topic, 'w')
@@ -156,4 +152,94 @@ def match_topic(topics_list, string):
             else:
                 #stats[topic] *= 1.0 / float(data['all_words']['count'])
                 stats[topic] *= 1.0 / float( all_struct['all_words']['count'] )
+       
     return stats
+
+
+
+ 
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+br.set_handle_equiv(True)
+br.set_handle_gzip(True)
+br.set_handle_redirect(True)
+br.set_debug_http(True)
+br.set_debug_redirects(True)
+cj = cookielib.LWPCookieJar()
+br.set_cookiejar(cj)
+
+
+def get_topics_and_words2(topic, dont_crawl, br):
+    try:
+        all_struct = json.loads( open('all_struct.txt', 'r').read() )
+    except IOError:
+        all_struct = {"all_words": {}}
+        all_struct["all_words"]["count"] = 0
+        # iteratively add words to this along the way and count
+        # them each time they are encountered once again
+        all_struct["all_words"]["words"] = {}
+    # use the words in here when calculating to find out the amount of times they have
+    # appeared in the document vs how many times they appeared in total
+    topic_struct = {"words": {}, "documents": [], "total_words": 0}
+    #new_data = {"words": {}, "documents": [], "total_words": 0}
+    seq = re.compile(r'[A-Za-z ]+[A-Za-z, ]+')
+    api = 'http://www.jstor.org/action/doBasicSearch?Query={0}&Search=Search&gw=jtx&prq=topic+identifier&hp=100&acc=off&aori=off&wc=on&fc=off'
+    br.open(api.format(topic))
+    with_info = [i for i in br.links() if 'http://www.jstor.org/stable' in i.absolute_url]
+    try:
+        for link in with_info:
+        
+            br.follow_link(link)
+            resp = br.response().read()
+            if 'An abstract for this item is not available' not in resp:
+                start = resp.find('<div class="abstract">')
+                _next = resp.find('\n', resp.find('<p>', start+1)+1)
+                end = resp.find('</div>', _next+1)
+                text = resp[_next:end]
+                text = text[10:]
+                #data["topic"][topic]["documents"].append(text)
+                topic_struct["documents"].append(text)
+                words_list = re.findall(seq, text)
+                if words_list != None and len(words_list) > 0:
+                    words_list = words_list[0].split(' ')
+                    for word in words_list:
+                        if word != '':
+                            word = word.lower()
+                            all_struct["all_words"]["count"] += 1
+                            #data["topic"][topic]["total_words"] += 1
+                            topic_struct["total_words"] += 1
+                       
+                            #f word not in data["all_words"]["words"].keys():
+                            if word not in all_struct["all_words"]["words"].keys():
+                                # data["all_words"]["words"][word] = {"number": 1, "topics": [topic]}
+                                all_struct["all_words"]["words"][word] = {"number": 1}
+                            else:
+                                # data["all_words"]["words"][word]["number"] += 1
+                                all_struct["all_words"]["words"][word]["number"] += 1
+                                if word in topic_struct["words"].keys():
+                                    #data["topic"][topic]["words"][word]["number"] += 1
+                                    topic_struct["words"][word]["number"] += 1
+                                else:
+                                    topic_struct["words"][word] = {"number": 1}
+            else:
+                pass
+    except:
+        f = open('not_crawled.txt', 'w')
+        if dont_crawl != None:
+            dont_crawl.append(topic)
+        else:
+            dont_crawl = [topic]
+        blob = {'topics': dont_crawl}
+        f.write(json.dumps(blob))
+        f.close()
+        print "%s gave an error and we added to not crawl list" % topic
+
+    f = open('/home/wes/Documents/projects/topic_classifier/topicstuff/topstructs/%s.txt' % topic, 'w')
+    f.write( json.dumps( topic_struct )  )
+    f.close()
+        
+    ff = open('all_struct.txt', 'w')
+    d = ff.write( json.dumps(all_struct) )
+    ff.close()
+    print "All %s written and all_struct updated" % topic
